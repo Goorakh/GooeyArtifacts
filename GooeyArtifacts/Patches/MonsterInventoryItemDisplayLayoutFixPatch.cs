d258f47301cbd8daab393925a8b32d06de78ac29
a22petoh <petter.ohlen@gmail.com>
2024-01-03 23:47:28 +0100

    Fix monster inventory display not updating size as items are added/removed

---
+++ GooeyArtifacts/Patches/MonsterInventoryItemDisplayLayoutFixPatch.cs
@@ -0,0 +1,25 @@
+﻿using RoR2;
+using RoR2.UI;
+using UnityEngine.UI;
+
+namespace GooeyArtifacts.Patches
+{
+    // Fixes the item container not resizing to fit the item icons until there is some other UI update on the info panel
+    static class MonsterInventoryItemDisplayLayoutFixPatch
+    {
+        [SystemInitializer]
+        static void Init()
+        {
+            On.RoR2.UI.ItemInventoryDisplay.UpdateDisplay += ItemInventoryDisplay_UpdateDisplay;
+        }
+
+        static void ItemInventoryDisplay_UpdateDisplay(On.RoR2.UI.ItemInventoryDisplay.orig_UpdateDisplay orig, ItemInventoryDisplay self)
+        {
+            orig(self);
+
+#pragma warning disable Publicizer001 // Accessing a member that was not originally public
+            LayoutRebuilder.MarkLayoutForRebuild(self.rectTransform);
+#pragma warning restore Publicizer001 // Accessing a member that was not originally public
+        }
+    }
+}
