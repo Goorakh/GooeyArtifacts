93dded6bd38abc48a958ee060b07b26fea8613fa
a22petoh <petter.ohlen@gmail.com>
2024-01-05 00:08:29 +0100

    Polish: Mostly fix interactables clipping into the floor/floating in the air if nodes don't match the terrain collision

--- GooeyArtifacts/EntityStates/MovingInteractables/MovingInteractableMoveToTargetState.cs
+++ GooeyArtifacts/EntityStates/MovingInteractables/MovingInteractableMoveToTargetState.cs
@@ -1,6 +1,9 @@
 ﻿using GooeyArtifacts.Utils;
 using RoR2;
+using RoR2.ConVar;
 using RoR2.Navigation;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace GooeyArtifacts.EntityStates.MovingInteractables
@@ -8,6 +11,8 @@ namespace GooeyArtifacts.EntityStates.MovingInteractables
     [EntityStateType]
     public class MovingInteractableMoveToTargetState : MovingInteractableBaseState
     {
+        static readonly BoolConVar _cvDrawPathData = new BoolConVar("goo_draw_interactable_paths", ConVarFlags.None, "0", "");
+
         public Vector3 Destination;
 
         Quaternion _startRotation;
@@ -24,9 +29,14 @@ namespace GooeyArtifacts.EntityStates.MovingInteractables
         Quaternion _currentRotation;
 
         Vector3 _targetPosition;
+        Vector3 _directTargetPosition;
         Quaternion _targetRotation;
 
         bool _targetAtEnd;
+
+        readonly List<DebugOverlay.MeshDrawer> _pathDrawers = new List<DebugOverlay.MeshDrawer>();
+        DebugOverlay.MeshDrawer _currentPositionVisualizer;
+        DebugOverlay.MeshDrawer _currentTargetPositionVisualizer;
 
         public override void OnEnter()
         {
@@ -106,6 +116,46 @@ namespace GooeyArtifacts.EntityStates.MovingInteractables
 
                         _currentPosition = transform.position;
                         _currentRotation = transform.rotation;
+
+                        if (_cvDrawPathData.value)
+                        {
+                            using WireMeshBuilder meshBuilder = new WireMeshBuilder();
+
+                            static DebugOverlay.MeshDrawer createOwnerMeshDrawer(WireMeshBuilder meshBuilder)
+                            {
+                                DebugOverlay.MeshDrawer drawer = DebugOverlay.GetMeshDrawer();
+                                drawer.hasMeshOwnership = true;
+                                drawer.mesh = meshBuilder.GenerateMesh();
+                                return drawer;
+                            }
+
+                            Vector3 previousPosition = _currentPosition;
+                            for (int i = 0; i < _path.waypointsCount; i++)
+                            {
+                                if (nodeGraph.GetNodePosition(_path[i].nodeIndex, out Vector3 position))
+                                {
+                                    meshBuilder.AddLine(previousPosition, Color.yellow, position, Color.yellow);
+                                    previousPosition = position;
+                                }
+                            }
+
+                            _pathDrawers.Add(createOwnerMeshDrawer(meshBuilder));
+
+                            meshBuilder.Clear();
+                            meshBuilder.AddLine(Vector3.zero, Color.green, Vector3.up, Color.green);
+                            meshBuilder.AddLine(Vector3.zero, Color.green, Vector3.forward, Color.green);
+                            meshBuilder.AddLine(Vector3.left, Color.green, Vector3.right, Color.green);
+
+                            _pathDrawers.Add(_currentTargetPositionVisualizer = createOwnerMeshDrawer(meshBuilder));
+
+                            meshBuilder.Clear();
+                            meshBuilder.AddLine(Vector3.zero, Color.red, Vector3.up, Color.red);
+                            meshBuilder.AddLine(Vector3.zero, Color.red, Vector3.forward, Color.red);
+                            meshBuilder.AddLine(Vector3.left, Color.red, Vector3.right, Color.red);
+
+                            _pathDrawers.Add(_currentPositionVisualizer = createOwnerMeshDrawer(meshBuilder));
+                        }
+
                         return;
                     }
                 }
@@ -128,9 +178,20 @@ namespace GooeyArtifacts.EntityStates.MovingInteractables
             {
                 PathTraveller.TravelData travelData = _pathTraveller.AdvancePosition(moveDelta);
 
-                _targetPosition = travelData.CurrentPosition;
+                _directTargetPosition = travelData.CurrentPosition;
+
+                Vector3 groundPosition = _directTargetPosition;
+                Vector3 groundNormal = travelData.InterpolatedNormal;
+
+                if (tryFindProperGroundPosition(travelData.CurrentPosition, -travelData.InterpolatedNormal, out Vector3 properGroundNormal, out Vector3 properGroundPosition))
+                {
+                    groundPosition = properGroundPosition;
+                    groundNormal = properGroundNormal;
+                }
+
+                _targetPosition = groundPosition;
                 _targetAtEnd = travelData.IsAtEnd;
-                _targetRotation = Util.QuaternionSafeLookRotation(travelData.Direction, travelData.InterpolatedNormal);
+                _targetRotation = Util.QuaternionSafeLookRotation(travelData.Direction, groundNormal);
 
                 estimatedTimeRemaining = travelData.RemainingTotalDistance / _moveSpeed;
             }
@@ -139,13 +200,17 @@ namespace GooeyArtifacts.EntityStates.MovingInteractables
                 estimatedTimeRemaining = 0f;
             }
 
+            _currentTargetPositionVisualizer?.transform.SetPositionAndRotation(_targetPosition, _targetRotation);
+
             float stepMagnitude = Mathf.Sqrt(Mathf.Min(1f, Util.Remap(fixedAge, 0f, 1f, 0f, 1f))
                                              * Mathf.Clamp01(Util.Remap(estimatedTimeRemaining, 0f, 2.5f, 0f, 1f)));
 
             float stepValue = 4f * Mathf.PI * fixedAge;
 
-            _currentPosition = Vector3.MoveTowards(_currentPosition, _targetPosition, moveDelta);
+            _currentPosition = Vector3.MoveTowards(_currentPosition, _targetPosition, (_moveSpeed + 0.25f) * Time.fixedDeltaTime);
             _currentRotation = Quaternion.RotateTowards(_currentRotation, _targetRotation, 135f * Time.fixedDeltaTime);
+
+            _currentPositionVisualizer?.transform.SetPositionAndRotation(_currentPosition, _currentRotation);
 
             transform.position = _currentPosition + (_currentRotation * new Vector3(0f, Mathf.Abs(Mathf.Sin(stepValue)) * _stepHeight * stepMagnitude, 0f));
             transform.rotation = Quaternion.AngleAxis(Mathf.Sin(stepValue + (Mathf.PI / 2f)) * _maxStepRotation * stepMagnitude, _currentRotation * Vector3.forward) * _currentRotation;
@@ -158,7 +223,7 @@ namespace GooeyArtifacts.EntityStates.MovingInteractables
                 outer.SetNextState(new MovingInteractableSettleState
                 {
                     TargetRotation = Quaternion.Euler(targetEuler),
-                    TargetPosition = _targetPosition
+                    TargetPosition = _directTargetPosition
                 });
             }
         }
@@ -169,6 +234,101 @@ namespace GooeyArtifacts.EntityStates.MovingInteractables
 
             _path?.Dispose();
             _path = null;
+
+            foreach (DebugOverlay.MeshDrawer drawer in _pathDrawers)
+            {
+                try
+                {
+                    drawer.Dispose();
+                }
+                catch (NullReferenceException)
+                {
+                    if (drawer.transform)
+                    {
+                        Destroy(drawer.transform.gameObject);
+                    }
+                }
+            }
+
+            _pathDrawers.Clear();
+
+            _currentPositionVisualizer = null;
+            _currentTargetPositionVisualizer = null;
+        }
+
+        bool tryFindProperGroundPosition(Vector3 position, Vector3 down, out Vector3 normal, out Vector3 properPosition)
+        {
+            float radius = HullDef.Find(hullSize).radius;
+
+            bool isValidCollision(Transform otherTransform)
+            {
+                if (otherTransform.IsChildOf(transform))
+                    return false;
+
+                EntityLocator entityLocator = otherTransform.GetComponentInParent<EntityLocator>();
+                if (entityLocator && entityLocator.entity && !isValidCollision(entityLocator.entity.transform))
+                    return false;
+
+                return true;
+            }
+
+            bool sphereCast(Ray ray, float maxDistance, out RaycastHit hit)
+            {
+                RaycastHit[] hits = Physics.SphereCastAll(ray, radius, maxDistance, LayerIndex.world.mask);
+
+                if (hits.Length == 0)
+                {
+                    hit = default;
+                    return false;
+                }
+
+                float closestHitDistance = float.PositiveInfinity;
+                RaycastHit closestHit = default;
+
+                foreach (RaycastHit hitCandidate in hits)
+                {
+                    // UnityDocs: For colliders that overlap the sphere at the start of the sweep, RaycastHit.normal is set opposite to the direction of the sweep, RaycastHit.distance is set to zero, and the zero vector gets returned in RaycastHit.point
+                    if (hitCandidate.distance <= 0f)
+                        continue;
+
+                    if (hitCandidate.distance >= closestHitDistance)
+                        continue;
+
+                    if (!isValidCollision(hitCandidate.transform))
+                        continue;
+
+                    closestHit = hitCandidate;
+                    closestHitDistance = hitCandidate.distance;
+                }
+
+                hit = closestHit;
+                return !float.IsInfinity(closestHitDistance);
+            }
+
+            RaycastHit hit;
+
+            const float SEARCH_INCREMENT = 0.5f;
+
+            for (int i = 1; i <= 50; i++)
+            {
+                if (sphereCast(new Ray(position + (-down * (i * SEARCH_INCREMENT)), down), SEARCH_INCREMENT * 1.05f, out hit))
+                {
+                    normal = hit.normal;
+                    properPosition = hit.point;
+                    return true;
+                }
+            }
+
+            if (sphereCast(new Ray(position + (-down * radius), down), radius * 3.5f, out hit))
+            {
+                normal = hit.normal;
+                properPosition = hit.point;
+                return true;
+            }
+
+            normal = -down;
+            properPosition = position;
+            return false;
         }
     }
 }
